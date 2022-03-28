4c5b3cfbb4990b95055c7bfbd17758ff8f686539
Sheharyaar <38083700+maliksheharyaar@users.noreply.github.com>
2022-03-27 22:29:31 -0400

    [PRWA-2]: Finished React side of weather application

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 const App = () => {
     const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
 
     const search = async (event) => {
         if(event.key === 'Enter'){
             const data = await fetchWeather(query)
-            console.log(data);
+            
+            setWeather(data); //Assign data to weather variable 
+            setQuery(''); //Reset the query
         }
     }
 
@@ -22,6 +25,25 @@ const App = () => {
                 onChange={(event) => setQuery(event.target.value)}
                 onKeyPress={search}
                 />
+
+            {weather.main && ( //If weather exists, return the component or else return nothing
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        { <sup>{weather.sys.country}</sup> /*HTML notation for superscript */}
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+
+                </div>
+            )}
            
         </div>
     );
