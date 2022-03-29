49bd6923edb903628bf23086576fda36f3884192
Sheharyaar <38083700+maliksheharyaar@users.noreply.github.com>
2022-03-28 20:41:58 -0400

    [PRWA-5]: Added 4 day forcast along with current day weather

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, {useState} from "react";
-import { fetchWeather } from "./api/fetchWeather";
+import { fetchForcast, fetchWeather } from "./api/fetchWeather";
 import './App.css';
 
 const App = () => {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
+    const [forcast, setForcast] = useState({});
+    const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    const d = new Date();
 
     const search = async (event) => {
         if(event.key === 'Enter'){
-            const data = await fetchWeather(query)
+            const weatherData = await fetchWeather(query);
             
-            setWeather(data); //Assign data to weather variable 
-            setQuery(''); //Reset the query
+            setWeather(weatherData); //Assign data to weather variable 
+            setQuery(''); //Reset query
+           
+
+            const forcastData = await fetchForcast(weatherData.coord.lat, weatherData.coord.lon);
+            setForcast(forcastData); //Assign forcastData to forcast variable
+            
+            
+            
         }
     }
 
@@ -26,8 +37,8 @@ const App = () => {
                 onChange={(event) => setQuery(event.target.value)}
                 onKeyPress={search}
                 />
-
-            {weather.main && ( //If weather exists, return the component or else return nothing
+           
+            {forcast.current && ( //If weather exists, return the component or else return nothing
                 <div className="city">
                     <h2 className="city-name">
                         <span>{weather.name}</span>
@@ -42,6 +53,101 @@ const App = () => {
                         <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
                         <p>{weather.weather[0].description}</p>
                     </div>
+
+
+
+
+                    <div className="forcast-grid">
+                        <div className="forcast-row">
+                            <h1 className="city-name">
+                                <span>{weekday[d.getDay()] + 1 === 7 ? weekday[0] : weekday[d.getDay()]}</span>
+                            </h1>
+
+                            <div className="day-night-temp-forcast">
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[0].temp.day)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[0].temp.night)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                            </div>
+
+                            <div className="info">
+                                <img className="forcast-city-icon" src={`https://openweathermap.org/img/wn/${forcast.daily[0].weather[0].icon}@2x.png`} alt={forcast.daily[0].weather[0].description} />
+                                <p>{forcast.daily[0].weather[0].description}</p>
+                            </div>
+                        </div>
+
+                        <div className="forcast-row">
+                            <h1 className="city-name">
+                                <span>{weekday[d.getDay()] + 2 === 7 ? weekday[0] : weekday[d.getDay() + 1]}</span>
+                            </h1>
+
+                            <div className="day-night-temp-forcast">
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[1].temp.day)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[1].temp.night)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                            </div>
+
+                            <div className="info">
+                                <img className="forcast-city-icon" src={`https://openweathermap.org/img/wn/${forcast.daily[1].weather[0].icon}@2x.png`} alt={forcast.daily[1].weather[0].description} />
+                                <p>{forcast.daily[1].weather[0].description}</p>
+                            </div>
+                        </div>
+
+                        <div className="forcast-row">
+                        <h1 className="city-name">
+                                <span>{weekday[d.getDay()] + 3 === 7 ? weekday[0] : weekday[d.getDay() + 2]}</span>
+                            </h1>
+
+                            <div className="day-night-temp-forcast">
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[2].temp.day)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[2].temp.night)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                            </div>
+
+                            <div className="info">
+                                <img className="forcast-city-icon" src={`https://openweathermap.org/img/wn/${forcast.daily[2].weather[0].icon}@2x.png`} alt={forcast.daily[2].weather[0].description} />
+                                <p>{forcast.daily[2].weather[0].description}</p>
+                            </div>
+                        </div>
+
+                        <div className="forcast-row">
+                        <h1 className="city-name">
+                                <span>{weekday[d.getDay()] + 4 === 7 ? weekday[0] : weekday[d.getDay() + 3]}</span>
+                            </h1>
+
+                            <div className="day-night-temp-forcast">
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[3].temp.day)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                                <div className="forcast-temp">
+                                    {Math.round(forcast.daily[3].temp.night)}
+                                    <sup>&deg;C</sup>
+                                </div>
+                            </div>
+
+                            <div className="info">
+                                <img className="forcast-city-icon" src={`https://openweathermap.org/img/wn/${forcast.daily[3].weather[0].icon}@2x.png`} alt={forcast.daily[3].weather[0].description} />
+                                <p>{forcast.daily[3].weather[0].description}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    
 
                 </div>
             )}
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,13 +1,29 @@
 import axios from "axios";
 
 
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const FORCAST_URL = 'https://api.openweathermap.org/data/2.5/onecall';
 const API_KEY = '0c9e2d492390de2df163240753df5365';
 
 export const fetchWeather = async (query) => {
-    const {data} = await axios.get(URL, {
+    const {data} = await axios.get(WEATHER_URL, {
         params: {
             q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
+}
+
+export const fetchForcast = async (forcastlat, forcastlon) => {
+    
+    const {data} = await axios.get(FORCAST_URL, {
+        params: {
+            lat: forcastlat,
+            lon: forcastlon,
+            exclude: 'minutely,alerts',
             units: 'metric',
             APPID: API_KEY,
         }
